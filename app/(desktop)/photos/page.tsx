bd67eb1aa94dbe915cf5415129e519e20dff10f1
James McFadden <jjlollipop6@gmail.com>
2026-04-27 18:28:54 -0500

    Remove Photos app

--- app/(desktop)/photos/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AppShellPage } from "@/lib/desktop/app-shell-page";
-
-export default function PhotosPage() {
-  return <AppShellPage appId="photos" />;
-}
--- components/apps/photos/index.ts
+++
@@ -1,1 +0,0 @@
-export { PhotosApp } from "./photos-app";
